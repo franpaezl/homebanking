e2e2c80f0d1506bbf41cad3e4ed31d8cb50ccd18
franpaezl <franciscopaezlastra@gmail.com>
2024-09-25 11:02:55 -0300

    aaa

--- src/main/java/com/minhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/minhub/homebanking/HomebankingApplication.java
@@ -44,7 +44,7 @@ public class HomebankingApplication {
 
             // Crear clientes
             Client client1 = new Client("Melba", "Morel", "melbamorel@hotmail.com", passwordEncoder.encode("melba123"));
-            Client client2 = new Client("Fran", "Paez", "fran.paez@admin.com", passwordEncoder.encode("melba123"));
+            Client client2 = new Client("Fran", "Paez", "fran.paez@hotmail.com", passwordEncoder.encode("melba123"));
             Client client3 = new Client("Pepe", "Pérez", "pepe.perez@example.com", passwordEncoder.encode("melba123"));
 
             clientRepository.save(client1);
